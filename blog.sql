97fe23856d72cd6b6e3848e50327aa904f95da23
Aman Singh Tomar <amansinghtomar2209@gmail.com>
2022-05-26 00:44:21 +0530

    databse

---
+++ blog.sql
@@ -0,0 +1,120 @@
+/*
+SQLyog Ultimate v9.02 
+MySQL - 5.0.24-community-nt : Database - blog-app-apis
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`blog-app-apis` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `blog-app-apis`;
+
+/*Table structure for table `categories` */
+
+DROP TABLE IF EXISTS `categories`;
+
+CREATE TABLE `categories` (
+  `category_id` int(11) NOT NULL auto_increment,
+  `description` varchar(255) default NULL,
+  `title` varchar(255) default NULL,
+  PRIMARY KEY  (`category_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `categories` */
+
+insert  into `categories`(`category_id`,`description`,`title`) values (3,'mobile phones','mobile'),(4,'Tv Models','TV');
+
+/*Table structure for table `hibernate_sequence` */
+
+DROP TABLE IF EXISTS `hibernate_sequence`;
+
+CREATE TABLE `hibernate_sequence` (
+  `next_val` bigint(20) default NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `hibernate_sequence` */
+
+insert  into `hibernate_sequence`(`next_val`) values (6);
+
+/*Table structure for table `post` */
+
+DROP TABLE IF EXISTS `post`;
+
+CREATE TABLE `post` (
+  `post_id` int(11) NOT NULL auto_increment,
+  `add_date` datetime default NULL,
+  `content` varchar(10000) default NULL,
+  `image_name` varchar(255) default NULL,
+  `post_title` varchar(255) default NULL,
+  `category_id` int(11) default NULL,
+  `user_id` int(11) default NULL,
+  PRIMARY KEY  (`post_id`),
+  KEY `FKjl0ab1c7s7gsd0tp830a7oogx` (`category_id`),
+  KEY `FK7ky67sgi7k0ayf22652f7763r` (`user_id`),
+  CONSTRAINT `FK7ky67sgi7k0ayf22652f7763r` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `FKjl0ab1c7s7gsd0tp830a7oogx` FOREIGN KEY (`category_id`) REFERENCES `categories` (`category_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `post` */
+
+insert  into `post`(`post_id`,`add_date`,`content`,`image_name`,`post_title`,`category_id`,`user_id`) values (1,'2022-05-16 03:32:42','Programming is using of concept and logic to automate','default.png','What is Programming',3,1),(2,'2022-05-16 03:35:39','Something used to communicate','default.png','What is Langue',3,1),(3,'2022-05-17 02:52:26','Something  communicate','default.png','What is ',4,3);
+
+/*Table structure for table `role` */
+
+DROP TABLE IF EXISTS `role`;
+
+CREATE TABLE `role` (
+  `id` int(11) NOT NULL auto_increment,
+  `name` varchar(255) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `role` */
+
+insert  into `role`(`id`,`name`) values (1,'role_admin');
+
+/*Table structure for table `user_role` */
+
+DROP TABLE IF EXISTS `user_role`;
+
+CREATE TABLE `user_role` (
+  `user` int(11) NOT NULL,
+  `role` int(11) NOT NULL,
+  PRIMARY KEY  (`user`,`role`),
+  KEY `FK26f1qdx6r8j1ggkgras9nrc1d` (`role`),
+  CONSTRAINT `FK26f1qdx6r8j1ggkgras9nrc1d` FOREIGN KEY (`role`) REFERENCES `role` (`id`),
+  CONSTRAINT `FKlduspqw8rg0gbcpludbfadw6l` FOREIGN KEY (`user`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `user_role` */
+
+insert  into `user_role`(`user`,`role`) values (1,1);
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `about` varchar(255) default NULL,
+  `email` varchar(255) default NULL,
+  `user_name` varchar(100) NOT NULL,
+  `password` varchar(255) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `users` */
+
+insert  into `users`(`id`,`about`,`email`,`user_name`,`password`) values (1,'I am programmer','amantomar@gmail.com','aman','$2a$10$PmEWSfyuPdVY5DqlntVoTuxpQFc.SPNgFglh2hBOs2kJTzgv/maAm'),(3,'I am chutiya','kavaynash@gmail.com','kavyansh','banniya');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
